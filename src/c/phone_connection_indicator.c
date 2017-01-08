12029968cecad3ba467cd19d794448a7147865e1
Markus Ressel <markus9100@gmail.com>
2017-01-08 05:21:56 +0100

    added connection indicator icon

--- src/c/phone_connection_indicator.c
+++ src/c/phone_connection_indicator.c
@@ -1,22 +1,53 @@
 #include <pebble.h>
 #include "phone_connection_indicator.h"
 #include "system_event_listener.h"
+#include <pebble-effect-layer/pebble-effect-layer.h>
+#include <pebble-gbitmap-lib/gbitmap_tools.h>
 #include "theme.h"
 
 // phone connection indicator layer
 static Layer *s_phone_connection_indicator_layer;
 
+static GBitmap *s_connection_icon;
+
+// Effect layer, covering the icon to change image color
+static EffectLayer *s_effect_layer;
+
 static void phone_connection_indicator_update_proc(Layer *layer, GContext *ctx) {
+  GRect layer_bounds = layer_get_bounds(layer);
+  
+  // draw background color (for correct color inversion)
+  graphics_context_set_fill_color(ctx, GColorWhite);
   GRect bounds = layer_get_bounds(layer);
+  graphics_fill_rect(ctx, bounds, 0, GCornerNone);
+  
+  if (s_connection_icon) {
+    // Set the compositing mode (GCompOpSet is required for transparency)
+    graphics_context_set_compositing_mode(ctx, GCompOpSet);
+    
+    GRect bounds = gbitmap_get_bounds(s_connection_icon);
+    
+    graphics_draw_bitmap_in_rect(
+      ctx,
+      s_connection_icon, 
+      (GRect) {
+        .origin = (GPoint) {
+          (layer_bounds.size.w - bounds.size.w)/2 - 3,
+          (layer_bounds.size.h - bounds.size.h)/2
+        },
+      .size=bounds.size
+      }
+    );                   
+  }
   
   if (is_phone_app_connected) {
-    // Fill it completely
-    graphics_context_set_fill_color(ctx, foregroundColor);
-    graphics_fill_rect(ctx, GRect(0, 0, bounds.size.w, bounds.size.h), 0, GCornerNone);
-  } else {
-    // Dont fill
-    graphics_context_set_stroke_color(ctx, foregroundColor);
-    graphics_draw_rect(ctx, GRect(0, 0, bounds.size.w, bounds.size.h));
+    graphics_context_set_stroke_color(ctx, GColorBlack);
+    graphics_context_set_stroke_width(ctx, 5);
+    
+    // draw cross over icon
+    graphics_draw_line(ctx, GPoint(5,5), GPoint(layer_bounds.size.w - 5, layer_bounds.size.h - 5));
+    graphics_draw_line(ctx, GPoint(5,layer_bounds.size.h - 5), GPoint(layer_bounds.size.w - 5, 5));
+    // graphics_draw_line(ctx, GPoint(0,layer_bounds.size.h / 2), GPoint(layer_bounds.size.w, layer_bounds.size.h / 2));
   }
 }
 
@@ -33,22 +64,40 @@ void create_phone_connection_indicator_layer(Window *window) {
   GRect bounds = layer_get_bounds(window_layer);
   
   // Create battery meter Layer
-  int width = 10;
-  int height = 10;
-  int offsetX = 5;
-  int offsetY = 5;
+  int width = 25;
+  int height = 25;
+  int offsetX = 0;
+  int offsetY = 0;
   GRect layer_bounds = GRect(offsetX, offsetY, width, height);
   s_phone_connection_indicator_layer = layer_create(layer_bounds);
   layer_set_update_proc(s_phone_connection_indicator_layer, phone_connection_indicator_update_proc);  
+  
+  s_connection_icon = gbitmap_create_with_resource(RESOURCE_ID_PHONE_CONNECTION_ICON);
+  s_connection_icon = scaleBitmap(s_connection_icon, 50, 50);
+  
+  // Create effect layer
+  s_effect_layer = effect_layer_create(layer_bounds);
+  
+  if (appTheme == DARK) {
+    // add color inversion effect
+    effect_layer_add_effect(s_effect_layer, effect_invert, NULL);
+  }
   
   // force update
   update_phone_connection_indicator();
   
   // Add to Window
   layer_add_child(window_get_root_layer(window), s_phone_connection_indicator_layer);
+  layer_add_child(window_layer, effect_layer_get_layer(s_effect_layer));
 }
 
 // destroy the layer
 void destroy_phone_connection_indicator_layer() {
+  // destroy icon
+  if(s_connection_icon) {
+    gbitmap_destroy(s_connection_icon);
+  }
+  
+  effect_layer_destroy(s_effect_layer);
   layer_destroy(s_phone_connection_indicator_layer);
 }
